1b555bc92203f6b08a0e1f04b1af7d2e05acbdef
skalaminhossain <77499874+skalaminhossain@users.noreply.github.com>
2021-01-16 00:51:48 +0600

    Create StoreManager.js

---
+++ StoreManager.js
@@ -0,0 +1,8 @@
+ function main() {
+    var i = parseInt(readLine(), 10);
+    var prices = [98.99, 15.2, 20, 1026];
+    //your code goes here
+    
+var newPrices = [98.99 + i, 15.2 + i, 20 + i, 1026 + i];
+    console.log(newPrices);
+}
